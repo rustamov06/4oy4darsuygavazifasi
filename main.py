275c7554b9a7359f17fac93f951f99522ccd5b3b
Rustamov <rustamovasilbek1221@gmail.com>
2024-12-05 22:15:00 +0500

    2vazifa

--- main.py
+++ main.py
@@ -27,5 +27,43 @@ print(f"Person name: {p1.name}")
 p2 = Product("SOFA")
 print(f"Product title: {p2.title}")
 p1.name = "MuRzo"
-print(f"Updated Person name: {p1.name}") 
+print(f"Updated Person name: {p1.name}")
+
+# 2 vazifa
+
+class PositiveValueDescriptor:
+    def __init__(self):
+        self._value = 0
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if isinstance(value, (int, float)) and value > 0:
+            self._value = value  # Musbat qiymat o'rnatiladi
+        else:
+            raise ValueError("Qiymat musbat son bo'lishi kerak!")
+    def __delete__(self, instance):
+        del self._value
+class BankAccount:
+    balance = PositiveValueDescriptor()
+    def __init__(self, balance):
+        self.balance = balance
+class Product:
+    price = PositiveValueDescriptor()
+    def __init__(self, price):
+        self.price = price
+try:
+    account = BankAccount(100)
+    print(f"Account balance: {account.balance}")
+    account.balance = 200
+    print(f"Updated balance: {account.balance}")
+    account.balance = -50
+except ValueError as e:
+    print(e)
+try:
+    product = Product(50)
+    print(f"Product price: {product.price}")
+    product.price = 0
+except ValueError as e:
+    print(e)
 
